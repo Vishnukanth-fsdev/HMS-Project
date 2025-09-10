84991791501da67ef29c3408c3f6ddd4fa3ca998
vishnukanthmyadav345 @gmail.com <vishnukanthmyadav5@gmail.com>
2025-09-10 12:50:59 +0530

    Login Implemented

---
+++ AuthService/src/main/java/com/authservice/userdto/LoginDto.java
@@ -0,0 +1,13 @@
+package com.authservice.userdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginDto {
+	
+	private String username;
+	private String password;
+
+}
